2068e3f0cdf4329695e0953e6498110987da0d78
Antoine D <dumoulinantoine@live.fr>
2021-05-31 11:51:20 +0200

    [0] #3, début de l'initialisation de la partie

---
+++ Projet Tendo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject TilePrefab;
+
+    private int _nbRow = 3;
+    private int _nbCol = 3;
+    private GameObject[,] _grid;
+    private Sprite _sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /** Initialisation **/
+        // TODO set sprite en fonction de l'OS
+
+        _grid = new GameObject[3,3];
+        for (int i = 0; i < _nbRow; i++)
+            for (int j = 0; i < _nbCol; j++)
+            {
+                _grid[i,j] = Instantiate(TilePrefab);
+                _grid[i, j].GetComponent<Tile>();
+            }
+    }
+
+    /*
+     * vérifie si la partie est terminée
+     */
+    public bool CheckWinCondition()
+    {
+        return false;   //TODO
+    }
+
+    /*
+     * vérifie si le coup peut être effectué
+     */
+    public bool CheckMoove()
+    {
+        return false;   //TODO
+    }
+}
---
+++ Projet Tendo/Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private GameManager _gameManager;
+    private int _index;
+    //private Texture2DArray _texture2D;
+
+    public void Instantiate(int index)
+    {
+        _index = index;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

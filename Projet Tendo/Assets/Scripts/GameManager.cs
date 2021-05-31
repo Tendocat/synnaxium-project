aba675a061913ec1e8a99bf00ffecf1ed43e4e33
Antoine D <dumoulinantoine@live.fr>
2021-05-31 18:32:34 +0200

    [0] #4, recherche de la tile en direction du drag

--- Projet Tendo/Assets/Scripts/GameManager.cs
+++ Projet Tendo/Assets/Scripts/GameManager.cs
@@ -1,6 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum Direction
+{
+    TOP,
+    BOT,
+    RIGHT,
+    LEFT
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -18,40 +28,87 @@ public class GameManager : MonoBehaviour
     {
         /** Initialisation **/
         // TODO set sprite en fonction de l'OS
+
         Rect cropRect = _sprite.rect;
-        SpriteRenderer tileSprite = null;
+        Image tileSprite;
+        Tile tileScript;
         float xStep = _sprite.rect.width/ _nbCol;
         float yStep = _sprite.rect.height/ _nbRow;
+        int index = 0;
+
+        Transform gridContainer = GameObject.Find("GridContainer").transform;
 
         _grid = new GameObject[_nbCol,_nbRow];
         for (int i = 0; i < _nbCol; i++)
             for (int j = 0; j < _nbRow; j++)
             {
-                _grid[i,j] = Instantiate(TilePrefab);
-                tileSprite = _grid[i, j].GetComponent<SpriteRenderer>();
+                _grid[i,j] = Instantiate(TilePrefab, gridContainer);
+                tileSprite = _grid[i, j].GetComponent<Image>();
+                tileScript = _grid[i, j].GetComponent<Tile>();
+                tileScript.Index = index++;
+                tileScript.col = i;
+                tileScript.row = j;
+
                 cropRect.x = i * xStep;
                 cropRect.y = j * yStep;
                 cropRect.xMax = (i+1) * xStep;
                 cropRect.yMax = (j+1) * yStep;
-                tileSprite.sprite = Sprite.Create(_sprite.texture, cropRect, new Vector2(0,1), 1);
-                _grid[i, j].transform.localScale = new Vector3(Scale, Scale, 0);
-                _grid[i, j].transform.position = new Vector3(i*xStep*Scale - Scale*xStep*_nbCol/2, j*yStep*Scale, 0);
+                tileSprite.sprite = Sprite.Create(_sprite.texture, cropRect, new Vector2(0,1), 100);
+                _grid[i, j].transform.position = new Vector3(i*3-3, j*3-3, 0);
             }
+        
+        //TODO mix puis disable 1
     }
 
-    /*
+    /**
+     * appelé par Tile lors du drag
+     */
+    public void TileEvent(Tile tile, Direction dir)
+    {
+        Tile newTile;
+        // TODO CheckMoove & get the other tile
+        Debug.Log(tile.Index + ":" + (((newTile=GetNextTile(tile, dir)) == null)? "out" : newTile.Index.ToString()));
+        
+    }
+    /**
+     * return the Tile next to origin toward dir or null if no any
+     */
+    private Tile GetNextTile(Tile origin, Direction dir)
+    {
+        int nextCol = origin.col;
+        int nextRow = origin.row;
+        switch (dir)
+        {
+            case Direction.TOP:
+                nextRow++;
+                break;
+            case Direction.BOT:
+                nextRow--;
+                break;
+            case Direction.RIGHT:
+                nextCol++;
+                break;
+            case Direction.LEFT:
+                nextCol--;
+                break;
+        }
+        if (nextCol < 0 || nextCol >= _nbCol || nextRow < 0 || nextRow >= _nbRow)
+            return null;
+        return _grid[nextCol, nextRow].GetComponent<Tile>();
+    }
+    /**
      * vérifie si la partie est terminée
      */
-    public bool CheckWinCondition()
+    private bool CheckWinCondition()
     {
         return false;   //TODO
     }
 
-    /*
-     * vérifie si le coup peut être effectué
+    /**
+     * swap les deux Tile
      */
-    public bool CheckMoove()
+    private void TileMoove(Tile a, Tile b)
     {
-        return false;   //TODO
+        // TODO
     }
 }
--- Projet Tendo/Assets/Scripts/Tile.cs
+++ Projet Tendo/Assets/Scripts/Tile.cs
@@ -1,21 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     private GameManager _gameManager;
     private int _index;
-    //private Texture2DArray _texture2D;
+    private Vector2 _originalMousePosition = Vector2.zero;
+    private bool _hasDrag = true;
 
-    public void Instantiate(int index)
+    public int col, row;
+    public int Index
     {
-        _index = index;
+        get => _index;
+        set
+        {
+            _index = value;
+            Text text = GetComponentInChildren<Text>();
+            text.text = _index.ToString();
+        }
+    }
+    void Start()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        if (_hasDrag)
+            return;
+        if (_originalMousePosition.magnitude - 50 > eventData.position.magnitude
+         || _originalMousePosition.magnitude + 50 < eventData.position.magnitude)
+        {
+            _hasDrag = true;
+            Direction dir = Direction.BOT;
+            Vector3 mouseDirection = eventData.position - _originalMousePosition;
+            if (Mathf.Abs(mouseDirection.x) < Mathf.Abs(mouseDirection.y))
+            {
+                if (mouseDirection.y > 0)
+                    dir = Direction.TOP;
+                else
+                    dir = Direction.BOT;
+            }
+            else
+            {
+                if (mouseDirection.x > 0)
+                    dir = Direction.RIGHT;
+                else
+                    dir = Direction.LEFT;
+            }
+            //Debug.Log(mouseDirection + " " + dir);
+            _gameManager.TileEvent(this, dir);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!_hasDrag)
+            return;
+        _hasDrag = false;
+        _originalMousePosition = eventData.position;
     }
 }

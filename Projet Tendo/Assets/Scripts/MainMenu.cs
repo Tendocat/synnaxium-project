f8a49c4cc2ed7a3b1fc49bde693bc19fc80bf7a8
Antoine D <dumoulinantoine@live.fr>
2021-05-31 10:39:58 +0200

    [0] #2, add main menu

---
+++ Projet Tendo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Exit()
+    {
+        Application.Quit(0);
+    }
+}
